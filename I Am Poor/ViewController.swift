c85ca984f7d1c85d70ae92056dca93ea2b36b8db
Benjamin Salter <bennsalter@gmail.com>
2020-08-26 10:47:01 -0400

    initial commit

---
+++ I Am Poor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Salter, Benn on 2/9/20.
+//  Copyright © 2020 Salter, Benn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
